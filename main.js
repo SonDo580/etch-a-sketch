70b9d4295a40e06548bfc2ee3f8e60258731a935
Son Do <dominhson580@gmail.com>
2022-07-11 16:59:39 +0700

    Modify getInput() checking logic

--- main.js
+++ main.js
@@ -18,21 +18,29 @@ const button = document.querySelector('button');
 button.addEventListener('click', changeGrid);
 
 function changeGrid() {
-    
+    let squaresPerSide = getInput();
 }
 
 function getInput() {
     // Keep asking until the user enter a valid number
     while (true) {
-        let input = prompt('Enter the number of squares per side (n <= 100)');
+        let input = prompt('Enter the number of squares per side (1 <= n <= 100)');
     
         if (input === null) {
             alert('You canceled!');
             break;
-        } else if (isNaN(+input)) {
-            alert('Please enter a valid number!')
         } else {
-            return +input;
+            input = +input;     // Convert to number
+
+            if (isNaN(input)
+                || Math.floor(input) !== input
+                || input < 1
+                || input > 100
+            ) {
+                alert('Please enter a valid number!');
+            } else {
+                return input;
+            }
         }
     }
 }
