ebef52db550c02eb7581096834e8ff9b7b4afb90
Son Do <dominhson580@gmail.com>
2022-07-11 15:20:25 +0700

    Append each row to the container

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const block = document.createElement('div');
         block.classList.add('block');
-        
+
         row.appendChild(block);
     }
+
+    container.appendChild(row);
 }
