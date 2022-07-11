cc3b19157d486d9f160da24127037ed2e437004f
Son Do <dominhson580@gmail.com>
2022-07-11 15:16:27 +0700

    Create a loop to create 16 rows

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const container = document.querySelector('.container');
 
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < 16; j++) {
+
+    }
+}
