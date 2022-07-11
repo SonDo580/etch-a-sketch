cbb328df7c482f98a46a3bea7d960cf9b97b32ea
Son Do <dominhson580@gmail.com>
2022-07-11 15:19:20 +0700

    Append 16 blocks to 1 row

--- main.js
+++ main.js
@@ -7,5 +7,7 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const block = document.createElement('div');
         block.classList.add('block');
+        
+        row.appendChild(block);
     }
 }
